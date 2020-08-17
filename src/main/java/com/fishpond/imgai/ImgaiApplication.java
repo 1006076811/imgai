1db1cab00673ee190f50b18ca847363487f0008c
Ryan <1006076811@qq.com>
2020-08-17 11:20:37 +0800

    初始化工程

---
+++ src/main/java/com/fishpond/imgai/ImgaiApplication.java
@@ -0,0 +1,13 @@
+package com.fishpond.imgai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgaiApplication.class, args);
+    }
+
+}
