d7bb095602a75e1f1f847aae968f5793b8c96c9a
Ryan <1006076811@qq.com>
2020-08-20 11:51:22 +0800

    更新前端: 新增了Axios的工具方法，并且引入到main

--- imgaiVue/imgaiVue/src/main.js
+++ imgaiVue/imgaiVue/src/main.js
@@ -3,26 +3,35 @@ import App from './App'
 import ElementUI from 'element-ui';
 import router from './router'
 import store from './store';
+import axios from 'axios';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/font/iconfont.css'
 
 
-Vue.config.productionTip = false
 
 
+
+// 挂载axios
+Vue.prototype.$http = axios
 Vue.use(ElementUI);
 Vue.use(store);
-// 设置访问根路径
-axios.defaults.baseURL = "http://localhost:8081"
 
+//axios的工具方法，之后用异步请求，直接用 this.xxx即可
 import {postRequest} from './utils/AxiosApi';
 import {getRequest} from "./utils/AxiosApi";
 import {putRequest} from "./utils/AxiosApi";
 import {deleteRequest} from "./utils/AxiosApi";
+
 Vue.prototype.postRequest = postRequest;
 Vue.prototype.getRequest = getRequest;
 Vue.prototype.putRequest = putRequest;
 Vue.prototype.deleteRequest = deleteRequest;
+// 设置访问根路径
+axios.defaults.baseURL = "http://localhost:8081"
+
+
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
